a5bf1ed1ca31098cf814803f48c52d7720993256
Benson Chang <bensonchang@damaiapp.com.tw>
2021-08-18 15:10:52 +0800

    chore: create Constants and add ExceptionConstant

---
+++ app/Constants/ExceptionConstant.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants;
+
+/**
+ * Class ExceptionConstant
+ *
+ * @package App\Constants
+ */
+class ExceptionConstant
+{
+    const SYSTEM_ERROR   = 500;
+    const FAILURE        = 400;
+    const ARGUMENT_ERROR = 400;
+    const NOT_FOUND      = 404;
+    const FORBIDDEN      = 403;
+    const UNAUTHORIZED   = 401;
+}
